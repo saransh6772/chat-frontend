8c1f16aa4539ff8676376e8e185e9dc550564417
Saransh <17107201s@gamil.com>
2024-03-07 23:38:42 +0530

    created admin login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ProtectedRoute from './components/auth/ProtectedRoute'
 import Loaders from './components/layout/Loaders'
 
+
 const Home = lazy(() => import('./pages/Home'))
 const Login = lazy(() => import('./pages/Login'))
 const Groups = lazy(() => import('./pages/Groups'))
 const Chat = lazy(() => import('./pages/Chat'))
 const NotFound = lazy(() => import('./pages/NotFound'))
+const AdminLogin = lazy(() => import('./pages/admin/AdminLogin'))
 let user = true;
 
 const App = () => {
@@ -21,6 +23,7 @@ const App = () => {
                         <Route path="/groups" element={<Groups />} />
                         <Route path="/chat/:chatId" element={<Chat />} />
                     </Route>
+                    <Route path='/admin' element={< AdminLogin/>}/>
                     <Route path='*' element={<NotFound />}></Route>
                 </Routes>
             </Suspense>
