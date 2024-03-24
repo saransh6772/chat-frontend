9cbd1faeafbbcdfda2cc0170e8bb85f789f449cd
Saransh <17107201s@gamil.com>
2024-03-24 10:42:08 +0530

    added redux store and created auth reduxers

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelmetProvider>
-            <CssBaseline />
-            <div onContextMenu={(e)=>e.preventDefault()}>
-                <App />
-            </div>
-        </HelmetProvider>
+        <Provider>
+            <HelmetProvider>
+                <CssBaseline />
+                <div onContextMenu={(e) => e.preventDefault()}>
+                    <App />
+                </div>
+            </HelmetProvider>
+        </Provider>
     </React.StrictMode>,
 )
