e8e4cf6981fb1d04c2f988aae856a00166de53e8
Saransh <17107201s@gamil.com>
2024-03-31 15:06:30 +0530

    changed color

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,6 +1,6 @@
 export const grayColor = "rgba(247,247,247,1)";
 export const lightBlue = "#2694ab";
 export const matBlack = "#1c1c1c";
-export const bgGradient = "linear-gradient(rgb(255 225 209), rgb(249 159 159))";
+export const bgGradient = "linear-gradient(rgba(255,235,255,0.9), rgba(250,160,160,0.9))";
 export const purple = "rgba(75,12,192,1)";
 export const purpleLight = "rgba(75,12,192,0.2)";
