0fb4871215d7edafaf2bdedb6fd7976cf92b8aca
Saransh <17107201s@gamil.com>
2024-03-31 19:17:03 +0530

    made some ui changes in admin

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -45,10 +45,10 @@ const Dashboard = () => {
                     {Appbar}
                     <Stack direction={{ xs: "column", lg: "row", }} flexWrap={"wrap"} justifyContent={"center"} alignItems={{ xs: "center", lg: "stretch", }} sx={{ gap: "2rem" }}>
                         <Paper elevation={3} sx={{ padding: "2rem", borderRadius: "1rem", width: "100%", maxWidth: "40rem", }}>
-                            <Typography variant="h4">Last Messages</Typography>
+                            <Typography variant="h4" m={'1rem'}>Last Messages</Typography>
                             <LineChart value={stats?.messagesChart || []} />
                         </Paper>
-                        <Paper elevation={3} sx={{ padding: "1rem", borderRadius: "1rem", display: "flex", justifyContent: "center", alignItems: "center", width: { xs: "100%", sm: "50%" }, position: "relative", maxWidth: "25rem", }}>
+                        <Paper elevation={3} sx={{ padding: "2rem", borderRadius: "1rem", display: "flex", justifyContent: "center", alignItems: "center", width: { xs: "100%", sm: "50%" }, position: "relative", maxWidth: "25rem", }}>
                             <DoughnutChart labels={["Single Chats", "Group Chats"]} value={[stats?.totalChatsCount - stats?.groupsCount || 0, stats?.groupsCount || 0,]} />
                             <Stack position={"absolute"} direction={"row"} justifyContent={"center"} alignItems={"center"} spacing={"0.5rem"} width={"100%"} height={"100%"}>
                                 <GroupIcon />
@@ -65,7 +65,7 @@ const Dashboard = () => {
 };
 
 const Widget = ({ title, value, Icon }) => (
-    <Paper elevation={3} sx={{ padding: "2rem", margin: "2rem 0", borderRadius: "1.5rem", width: "20rem", }}>
+    <Paper elevation={3} sx={{ padding: "1rem", margin: "1rem", borderRadius: "1.5rem", width: "20rem", }}>
         <Stack alignItems={"center"} spacing={"1rem"}>
             <Typography sx={{ color: "rgba(0,0,0,0.7)", borderRadius: "50%", border: `5px solid ${matBlack}`, width: "5rem", height: "5rem", display: "flex", justifyContent: "center", alignItems: "center", }}>{value}</Typography>
             <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
